4960419307fa11b996218038995e4f2e1488a71c
hanh2002 <hanh.dinh@lobstertechnologies.net>
2023-12-27 15:57:48 +0700

    test ci cd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World 12345");
 });
 
 app.listen(port, function (error) {
